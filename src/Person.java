edd1bf77665786c7a19d7a9d77c3eee9b50bb6da
Alisher12398 <Alisher12398@gmail.com>
2018-07-12 14:55:35 +0600

    Add Person class

---
+++ src/Person.java
@@ -0,0 +1,41 @@
+import java.util.Date;
+
+public class Person {
+    private String name;
+    private String surname;
+    private int age;
+    private Date birthday;
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public void setSurname(String surname){
+        this.surname=surname;
+    }
+
+    public void setAge(int age){
+        this.age=age;
+    }
+
+    public void setBirthday(Date birthday){
+        this.birthday=birthday;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getSurname(){
+        return surname;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public Date getBirthday(){
+        return birthday;
+    }
+
+}
